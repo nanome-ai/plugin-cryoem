34c35ddbccd2052bd815237a00a90bd2b8c74311
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-09-29 12:18:09 -0400

    simple load map and model

--- plugin/CryoEM.py
+++ plugin/CryoEM.py
@@ -1,7 +1,13 @@
 import os
 import tempfile
 import nanome
-from nanome.util import Logs, async_callback, enums
+import numpy as np
+import mcubes
+import pyfqmr
+import gzip
+
+from nanome.api.shapes import Mesh
+from nanome.util import Logs, enums, Vector3, Color, async_callback
 from iotbx.data_manager import DataManager
 from iotbx.map_model_manager import map_model_manager
 
@@ -20,31 +26,105 @@ class CryoEM(nanome.AsyncPluginInstance):
 
     async def load_map_and_model(self):
         Logs.message("Loading Map file")
-        map_file = "emd_30288.map.gz"
+        map_gz_file = "emd_30288.map.gz"
         pdb_file = "7c4u.pdb"
         dm = DataManager()
         dm.set_overwrite(True)
-        mm = dm.get_real_map(map_file)
-        Logs.message("loaded map")
+
+        mrc_file = tempfile.NamedTemporaryFile(suffix='.mrc')
+        mrc_filepath = mrc_file.name
+        with gzip.open(map_gz_file, 'rb') as f:
+            with open(mrc_filepath, 'wb') as out:
+                out.write(f.read())
+        mm = dm.get_real_map(mrc_filepath)
         model = dm.get_model(pdb_file)
-        Logs.message("loaded model")
+
         mmm = map_model_manager(
             model=model,
             map_manager=mm
         )
+
         selection_string = 'resseq 1:1000'
         box_mmm = mmm.extract_all_maps_around_model(selection_string=selection_string)
         # Write boxed residue range to files
-        boxed_map_filename = os.path.join(self.temp_dir.name, os.path.basename(map_file))
+        boxed_map_filename = os.path.join(self.temp_dir.name, os.path.basename(mrc_filepath))
         dm.write_real_map_file(
             box_mmm.map_manager(),
             filename=boxed_map_filename)
+
         boxed_model_filename = os.path.join(self.temp_dir.name, os.path.basename(pdb_file))
         dm.write_model_file(
             box_mmm.model(),
             filename=boxed_model_filename,
             extension="pdb")
+        # Generate mesh to upload to Nanome
+        iso = 0.1
+        opacity = 0.65
+        color_scheme = enums.ColorScheme.BFactor
+        mesh = self.generate_mesh(box_mmm.map_manager(), iso, color_scheme, opacity)
+
         await self.send_files_to_load([boxed_model_filename])
+        comp = (await self.request_complex_list())[0]
+        anchor = mesh.anchors[0]
+        anchor.anchor_type = enums.ShapeAnchorType.Complex
+        anchor.target = comp.index
+        await mesh.upload()
+
+    def generate_mesh(self, map_manager, iso, color_scheme, opacity=0.65, decimation_factor=5):
+        # Compute iso-surface with marching cubes algorithm
+        # self.set_limited_view_on_cog()
+        map_data = map_manager.map_data().as_numpy_array()
+        vertices, triangles = mcubes.marching_cubes(map_data, iso)
+        np_vertices = np.asarray(vertices)
+        np_triangles = np.asarray(triangles)
+        Logs.debug("Decimating mesh")
+        target = max(1000, len(np_triangles) / decimation_factor)
+        mesh_simplifier = pyfqmr.Simplify()
+        mesh_simplifier.setMesh(np_vertices, np_triangles)
+        mesh_simplifier.simplify_mesh(
+            target_count=target, aggressiveness=7, preserve_border=True, verbose=0
+        )
+        vertices, triangles, normals = mesh_simplifier.getMesh()
+        voxel_sizes = map_manager.pixel_sizes()
+        if voxel_sizes[0] > 0.0001:
+            Logs.debug("Setting voxels")
+            voxel_size = np.array(voxel_sizes)
+            vertices *= voxel_size
+
+        # Logs.debug("Limiting View")
+        # vertices, normals, triangles = self.limit_view(
+        #     (vertices, normals, triangles),
+        #     self.position,
+        #     self.radius,
+        # )
+
+        Logs.debug("Setting computed values")
+        computed_vertices = np.array(vertices)
+        computed_normals = np.array(normals)
+        computed_triangles = np.array(triangles)
+        mesh = Mesh()
+        mesh.vertices = computed_vertices.flatten()
+        mesh.normals = computed_normals.flatten()
+        mesh.triangles = computed_triangles.flatten()
+
+        # anchor = mesh.anchors[0]
+        # anchor.anchor_type = enums.ShapeAnchorType.Workspace
+        # anchor.local_offset = Vector3(
+        #     self._map_origin[0], self._map_origin[1], self._map_origin[2])
+        # anchor.local_offset = Vector3(0, 0, 0)
+
+        mesh.color = Color(255, 255, 255, int(opacity * 255))
+
+        # if self.nanome_complex is not None:
+        #     anchor.anchor_type = enums.ShapeAnchorType.Complex
+        #     anchor.target = self.nanome_complex.index
+
+        # if self.wireframe_mode:
+        #     self.wire_vertices, self.wire_normals, self.wire_triangles = self.wireframe_mesh()
+        #     mesh.vertices = np.asarray(self.wire_vertices).flatten()
+        #     mesh.triangles = np.asarray(self.wire_triangles).flatten()
+        # self.color_by_scheme(mesh, color_scheme)
+        return mesh
 
     def on_stop(self):
         self.temp_dir.cleanup()
@@ -81,10 +161,10 @@ class CryoEM(nanome.AsyncPluginInstance):
 
     async def render_mesh(self, map_group: map_model_manager):
         self.set_plugin_list_button(enums.PluginListButtonType.run, "Running...", False)
-        iso = map_group.isovalue
-        opacity = map_group.opacity
-        color_scheme = map_group.color_scheme
-
+        iso = 0.1
+        opacity = 0.65
+        self.radius = 15
+        color_scheme = enums.ColorScheme.BFactor
         comps = await self.request_complex_list()
         if comps:
             deep_comp = await self.request_complexes([comps[0].index])
@@ -92,7 +172,7 @@ class CryoEM(nanome.AsyncPluginInstance):
         else:
             map_group.nanome_complex = None
         Logs.message(f"Generating iso-surface for iso-value {round(iso, 3)}")
-        mesh = map_group.generate_mesh(iso, color_scheme, opacity)
+        mesh = self.generate_mesh(mm, iso, color_scheme, opacity)
         Logs.message(f"Uploading iso-surface ({len(mesh.vertices)} vertices)")
         await mesh.upload()
         self.set_plugin_list_button(enums.PluginListButtonType.run, "Run", True)
