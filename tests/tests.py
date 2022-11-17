6814ddb1fa0c359ab55c30049420b9fc7ff45a66
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2022-11-16 20:50:50 -0800

    Update Unittests (#14)

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,9 @@
 import asyncio
 import os
 import unittest
+from nanome.api import structure, PluginInstance
 from unittest.mock import MagicMock, patch
+from iotbx.map_manager import map_manager
 
 from mmtbx.model.model import manager
 from plugin.models import MapGroup, MapMesh
@@ -22,6 +24,7 @@ class MapGroupTestCase(unittest.TestCase):
 
     def setUp(self):
         self.plugin = MagicMock()
+        PluginInstance._instance = self.plugin
         self.map_group = MapGroup(self.plugin)
         self.pdb_file = os.path.join(fixtures_dir, '7c4u.pdb')
         self.map_file = os.path.join(fixtures_dir, 'emd_30288.map.gz')
@@ -30,37 +33,34 @@ class MapGroupTestCase(unittest.TestCase):
         self.map_group.add_pdb(self.pdb_file)
         self.assertTrue(isinstance(self.map_group._model, manager))
 
-    # @patch('nanome._internal._shapes._shape._Shape')
-    @patch('nanome._internal._network.PluginNetwork._instance')
-    @patch('plugin.models.MapGroup._plugin')
-    @patch('nanome.api.plugin_instance.PluginInstance._instance')
-    @unittest.skip("Need to figure out mocking. =(")
-    def test_add_map_gz(self, _instance_mock, plugin_mock, *mocks):
+    def test_add_map_gz(self):
         async def validate_add_map_gz(self):
+            # Set future result for request_complexes mock
+            fut = asyncio.Future()
+            fut.set_result([structure.Complex()])
+            self.plugin.add_to_workspace.return_value = fut
+            # run add_map_gz, and make sure map_manager is created on internal map_manager
+            self.assertTrue(isinstance(self.map_group.map_mesh.map_manager, type(None)))
             await self.map_group.add_map_gz(self.map_file)
-            self.assertTrue(isinstance(self.map_group.map_mesh, MapMesh))
-
-        # Set future result for request_complexes mock
-        fut = asyncio.Future()
-        fut.set_result([])
-        breakpoint()
-        plugin_mock.add_to_workspace.return_value = fut
+            self.assertTrue(isinstance(self.map_group.map_mesh.map_manager, map_manager))
         run_awaitable(validate_add_map_gz, self)
 
-    @patch('nanome._internal._network.PluginNetwork._instance')
-    @patch('nanome.api.plugin_instance.PluginInstance', return_value=asyncio.Future())
-    @unittest.skip("Need to figure out mocking. =(")
-    def test_generate_mesh(self, instance_mock, plugin_mock, *mocks):
+    @patch('nanome._internal._network.PluginNetwork._instance', return_value=asyncio.Future())
+    def test_generate_mesh(self, instance_mock):
         # Assert that attributes are set after load_map called.
         async def validate_generate_mesh(self):
-            await self.map_group.add_map_gz(self.map_file)
-            self.assertEqual(len(self.map_group.map_mesh.vertices), 0)
+            fut = asyncio.Future()
+            fut.set_result([structure.Complex()])
+            self.plugin.add_to_workspace.return_value = fut
+
+            map_file = os.path.join(fixtures_dir, 'emd_30288.map.gz')
+            expected_vertices = 4364
+            # Make sure vertices are added to mesh
+            self.assertEqual(len(self.map_group.map_mesh.computed_vertices), 0)
+            await self.map_group.add_map_gz(map_file)
             await self.map_group.generate_mesh()
-            self.assertTrue(len(self.map_group.map_mesh.computed_vertices) > 0)
-        plugin_mock.add_to_workspace.return_value = asyncio.Future()
-        with patch('plugin.models.MapGroup._plugin', plugin_mock) as m1:
-            with patch('nanome.api.plugin_instance.PluginInstance._instance') as m2:
-                run_awaitable(validate_generate_mesh, self)
+            self.assertEqual(len(self.map_group.map_mesh.computed_vertices), expected_vertices)
+        run_awaitable(validate_generate_mesh, self)
         # run_awaitable(validate_generate_mesh, self)
 
     # def test_toggle_wireframe_mode(self):
@@ -70,3 +70,49 @@ class MapGroupTestCase(unittest.TestCase):
     #     self.assertEqual(self.map_group.wireframe_mode, True)
     #     self.map_group.toggle_wireframe_mode(False)
     #     self.assertEqual(self.map_group.wireframe_mode, False)
+
+
+class MapMeshTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.plugin = MagicMock()
+        self.pdb_file = os.path.join(fixtures_dir, '7c4u.pdb')
+        self.map_file = os.path.join(fixtures_dir, 'emd_30288.map.gz')
+        self.map_mesh = MapMesh(self.plugin)
+
+    def test_add_map_gz_file(self):
+        # Set future result for request_complexes mock
+        fut = asyncio.Future()
+        fut.set_result([structure.Complex()])
+        self.plugin.add_to_workspace.return_value = fut
+        # run add_map_gz, and make sure map_manager is created on internal map_manager
+        self.assertTrue(isinstance(self.map_mesh.map_manager, type(None)))
+        self.map_mesh.add_map_gz_file(self.map_file)
+        self.assertTrue(isinstance(self.map_mesh.map_manager, map_manager))
+
+    def test_load(self):
+        async def validate_load(self):
+            """Validate that running load() generates the NanomeMesh."""
+            map_file = os.path.join(fixtures_dir, 'emd_30288.map.gz')
+            expected_vertices = 4437
+            expected_normals = 4437
+            expected_triangles = 7986
+            self.map_mesh.add_map_gz_file(map_file)
+            isovalue = 0.2
+            opacity = 0.65
+            radius = 5
+            position = 0.1
+
+            fut = asyncio.Future()
+            fut.set_result([structure.Complex()])
+            self.plugin.add_to_workspace.return_value = fut
+            mesh = self.map_mesh.mesh
+            self.assertEqual(len(mesh.vertices), 0)
+            self.assertEqual(len(mesh.normals), 0)
+            self.assertEqual(len(mesh.triangles), 0)
+            await self.map_mesh.load(isovalue, opacity, radius, position)
+            self.assertEqual(len(mesh.vertices), expected_vertices)
+            self.assertEqual(len(mesh.normals), expected_normals)
+            self.assertEqual(len(mesh.triangles), expected_triangles)
+        run_awaitable(validate_load, self)
