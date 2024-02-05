8d0c9d99d44b8e4d9be7cf1b57cc8dc8fe4a956c
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2024-02-05 11:53:43 -0500

    Fix removing existing containers (#44)

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,7 +3,7 @@
 echo "./deploy.sh $*" > redeploy.sh
 chmod +x redeploy.sh
 
-existing=$(docker ps -aqf name=cryo-em)
+existing=$(docker ps -q -f name='(cryo-em|cryoem)')
 if [ -n "$existing" ]; then
     echo "removing existing container"
     docker rm -f $existing
