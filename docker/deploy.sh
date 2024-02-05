d5641432777defe4ba858962e782e1c7ef7048e3
Mike Rosengrant <mike.rosengrant@nanome.ai>
2024-02-05 11:41:22 -0500

    remove dash again

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,7 +3,7 @@
 echo "./deploy.sh $*" > redeploy.sh
 chmod +x redeploy.sh
 
-existing=$(docker ps -aqf name=cryo-em)
+existing=$(docker ps -aqf name=cryoem)
 if [ -n "$existing" ]; then
     echo "removing existing container"
     docker rm -f $existing
